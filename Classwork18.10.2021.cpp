b408b9ef36d0c40a2dc84330ac66fc268e2466ac
MilkisWay <92961716+MilkisWay@users.noreply.github.com>
2021-10-22 22:48:56 +0300

    Add files via upload

---
+++ Classwork18.10.2021.cpp
@@ -0,0 +1,43 @@
+﻿// Classwork18.10.2021.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void fibonacci(int x,int y,vector<int> &v,int n)
+
+{
+    v.clear();
+    v.push_back(x);
+    v.push_back(y);
+    n = n - 1;
+    for (int i = 2; i < n-1; i++)
+    {
+        v.push_back(v[i] + v[i + 1]); //опасная запись
+    }
+     
+}
+
+void print(const vector<int> &v,const string &metka)
+{
+        for (int i = 0; i < v.size(); i++)
+        {
+
+            cout <<metka<<" "<<v[i] << endl;
+        }
+}
+
+int main()
+{
+    int n = 40;
+    string metka = "Output:";
+    vector <int> v;
+    fibonacci(1, 2, v, n);
+    print(v, metka);
+
+    return 0;
+
+}
+
